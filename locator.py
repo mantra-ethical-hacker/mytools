27c1ef42423ea11fd1375f54c565cc2baf7c5bd8
mantra-ethical-hacker <68106714+mantra-ethical-hacker@users.noreply.github.com>
2020-07-10 15:32:05 +0530

    Add files via upload

---
+++ locator.py
@@ -0,0 +1,20 @@
+import os 
+import urllib2
+import json
+
+while True:
+       ip = raw_input(" Enter the target IP : ")
+       url = "http://ip-api.com/json/"
+       response = urllib2.urlopen(url + ip) 
+       data = response.read()
+       values = json.loads(data)
+       
+       print(" IP " + values['query'])
+       print(" City " + values['city'])
+       print(" ISP " + values['isp'])
+       print(" Country " + values['counrty'])
+       print(" Region " + values['region'])
+       print(" Time Zone " + values['timezone'])
+       
+       break
+       
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Hello 
+This is simple script written in python
+Visit my YouTube Channel for more Hacking related content
+link = https://youtube.com/c/DevilsDoorGroup
